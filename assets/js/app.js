27a134fbe848a4fd72d6634d13a8f8904c36140f
Shariful Islam <78491249+Naimkhan567@users.noreply.github.com>
2021-07-13 20:27:15 +0600

    Creating a feedback app
     All file

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+
+
+//dropdown
+
+function dp() {
+    const dropDown = document.getElementById("dropDown");
+    dropDown.options[dropDown.selectedIndex];
+};
+
+
+//toggle
+
+const sideBar = document.querySelector('.sidebar');
+const toggleBtn = document.querySelector('.toggle-btn');
+
+
+toggleBtn.addEventListener("click", function() {
+    sideBar.classList.toggle('active');
+});
+
+
+
+
+
